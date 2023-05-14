e36a29b5325cf0e4be06dc4cf8b77da5f6a76147
Jai Lakhmani <jailakhmani12345@gmail.com>
2023-05-15 01:29:01 +0530

    Update [id].js

--- src/pages/task/[id].js
+++ src/pages/task/[id].js
@@ -22,18 +22,18 @@ const Tasks = () => {
   return (
     <div className="flex flex-col border h-screen  bg-white items-center">
       <div className="flex flex-col w-full  sm:w-3/5 items-center justify-center border p-10 pt-20 mt-10">
-        <div className="w-full lg:w-4/5 flex sm:space-x-4 sm:flex-row flex-col items-center justify-start">
-          <h1 className=" font-semibold text-xl">Id of the task :</h1>
+        <div className="w-full lg:w-4/5 flex sm:flex-row flex-col items-center justify-start">
+          <h1 className="sm:w-2/3 lg:w-1/2 font-semibold text-xl">Id of the task :</h1>
           {data && <p className="  ">{data.id}</p>}
         </div>
-        <div className="w-full lg:w-4/5 flex sm:space-x-4 sm:flex-row flex-col items-center justify-start">
-          <h1 className=" lg:w-1/2 font-semibold text-xl">
+        <div className="w-full lg:w-4/5 flex  sm:flex-row flex-col items-center justify-start">
+          <h1 className="sm:w-2/3 lg:w-1/2 font-semibold text-xl">
             Timing of the task :
           </h1>
           {data && <p className="  lg:w-1/2">{data.time}</p>}
         </div>
-        <div className="w-full lg:w-4/5 flex sm:space-x-4 sm:flex-row flex-col items-center justify-start">
-          <h1 className="lg:w-1/2 font-semibold text-xl">The task :</h1>
+        <div className="w-full lg:w-4/5 flex  sm:flex-row flex-col items-center justify-start">
+          <h1 className="sm:w-2/3 lg:w-1/2 font-semibold text-xl">The task :</h1>
           {data && <p className="  lg:w-1/2">{data.msg}</p>}
         </div>
       <button className="border px-4 py-2 hover:bg-blue-200 my-4 rounded-md" onClick={()=>deleteTask(data.id)}>Delete</button>
