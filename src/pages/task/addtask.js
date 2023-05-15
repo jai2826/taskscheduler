84007c87db2cec0c437df3f9b2214bb38d494514
Jai Lakhmani <jailakhmani12345@gmail.com>
2023-05-15 11:03:10 +0530

    Responsiveness INcrease

--- src/pages/task/addtask.js
+++ src/pages/task/addtask.js
@@ -29,9 +29,10 @@ const Addtask = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const length = tasks==null?0:tasks.length+1
+    const length = tasks == null ? 0 : tasks.length + 1;
+    const randomNum = Math.random()*1000000;
     const data = {
-      id: length,
+      id: Math.floor(randomNum),
       time: timeRange.start + " - " + timeRange.end,
       msg: taskmsg,
     };
@@ -40,27 +41,31 @@ const Addtask = () => {
   };
 
   return (
-    <div className=" h-screen w-full p-10 border justify-center flex pt-20">
+    <div className=" h-screen w-full p-1 md:p-10 border justify-center flex pt-8 md:pt-12 lg:pt-16">
       <form
-        className="flex flex-col h-full w-4/5  border p-10 rounded-md items-center space-y-10"
+        className="flex flex-col h-full md:w-4/5  w-full border p-8 lg:p-10 rounded-md items-center space-y-10"
         onSubmit={(e) => handleSubmit(e)}
       >
-        <div className="space-y-2 flex flex-col w-2/5">
+        <div className="space-y-2 flex flex-col w-full  lg:w-3/5 items-start">
           <h1 className="text-2xl font-semibold">Enter the Time</h1>
-          <div className="flex space-x-6 items-center">
+          <div className="flex flex-col md:flex-row md:space-x-6 items-start">
             <div className="flex space-x-4 h-fit ">
-              <label htmlFor="starttime">Start Time</label>
+              <label className="w-20" htmlFor="starttime">
+                Start Time
+              </label>
               <input
-                className="border"
+                className="border w-20"
                 type="time"
                 name="starttime"
                 onChange={(e) => handleStartTime(e)}
               />
             </div>
             <div className="flex space-x-4 h-fit">
-              <label htmlFor="endtime">End Time</label>
+              <label className="w-20" htmlFor="endtime">
+                End Time
+              </label>
               <input
-                className="border"
+                className="border w-20"
                 onChange={(e) => handleEndTime(e)}
                 type="time"
                 name="endtime"
@@ -68,17 +73,16 @@ const Addtask = () => {
             </div>
           </div>
         </div>
-        <div className="space-y-2 flex flex-col justify-items-start w-2/5">
+        <div className="space-y-2 flex flex-col w-full  lg:w-3/5 items-start">
           <label htmlFor="task" className="text-2xl font-semibold ">
             Enter the Name of the Task
           </label>
-          <div className="flex space-x-4 h-fit">
-            <textarea
-              className="border w-full"
-              onChange={(e) => handleTask(e)}
-              name="task"
-            />
-          </div>
+          <textarea
+            className="border w-full h-20"
+            onChange={(e) => handleTask(e)}
+            name="task"
+            
+          />
         </div>
         <button
           className="hover bg-blue-200 border rounded-md py-2 px-4"
